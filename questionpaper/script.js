c052a1abfc3489e9de16c05117716a981a5a7df8
creaneschool <132559104+creaneschool@users.noreply.github.com>
2023-05-06 17:28:46 +0530

    Add files via upload

---
+++ questionpaper/script.js
@@ -0,0 +1,12 @@
+function pw(){
+	window.open("pw.pdf")
+}
+function iit(){
+	window.open("iit.pdf")
+}
+function cbse(){
+	window.open("cbse.pdf")
+}
+function byjus(){
+	window.open("byjus.pdf")
+}
