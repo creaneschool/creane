18b0521dc39abe33ebb905eb02afef16c96b8b4b
creaneschool <creaneschoolnotes@gmail.com>
2023-06-17 21:18:40 +0530

    react started with ./

--- chat/frontend/src/jsx-js/main.js
+++ chat/frontend/src/jsx-js/main.js
@@ -1,5 +1,5 @@
 import React from './node_modules/react';
-import ReactDOM from 'react-dom';
+import ReactDOM from './node_module/react-dom';
 import App from './App.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
