4b495f793e516119122de1299d330263241cf2fb
creaneschool <creaneschoolnotes@gmail.com>
2023-06-20 11:38:47 +0530

    CHAT -> x

--- chat/frontend/src/jsx-js/main.js
+++
@@ -1,5 +0,0 @@
-import React from './node_modules/react';
-import ReactDOM from './node_module/react-dom';
-import App from './App.js';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
